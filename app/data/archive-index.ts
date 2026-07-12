e8b4f7799b8433b8c0e33e1fdee9d5202752d087
Franky Lok <frankylok@users.noreply.github.com>
2026-07-13 05:16:01 +0800

    Update daily tech news for 2026-07-13

--- app/data/archive-index.ts
+++ app/data/archive-index.ts
@@ -1,14 +1,17 @@
-import archive0 from "./archive/2026-07-12.json";
-import archive1 from "./archive/2026-07-11.json";
-import archive2 from "./archive/2026-07-10.json";
+import archive0 from "./archive/2026-07-13.json";
+import archive1 from "./archive/2026-07-12.json";
+import archive2 from "./archive/2026-07-11.json";
+import archive3 from "./archive/2026-07-10.json";
 
 export const archives = {
-  "2026-07-12": archive0,
-  "2026-07-11": archive1,
-  "2026-07-10": archive2
+  "2026-07-13": archive0,
+  "2026-07-12": archive1,
+  "2026-07-11": archive2,
+  "2026-07-10": archive3
 } as const;
 
 export const archiveDates = [
+  "2026-07-13",
   "2026-07-12",
   "2026-07-11",
   "2026-07-10"
